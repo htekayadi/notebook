6528729179105199f04fa8536096ae7b5bb27385
Hendrik Tekayadi <hendrik.tekayadi@gmail.com>
2016-11-06 17:45:08 +0700

    Add note views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,14 +1,26 @@
 class NotesController < ApplicationController
-  def index
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @notes = Note.all.order("created_at DESC")
   end
 
   def show
 
   end
 
-  def create
+  def new
+    @note = Note.new
+  end
 
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -26,10 +38,10 @@ class NotesController < ApplicationController
   private
 
   def find_note
-
+    @note = Note.find(params[:id])
   end
 
   def note_params
-
+    params.required(:note).permit(:title, :content)
   end
 end
